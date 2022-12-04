7d511b5eb79e6ee74fe9a8350679a76c12868ec5
Stef Graces <stefgraces@hotmail.com>
2022-12-04 22:45:52 +0100

    Revert spec stuff

--- internal/replicator/model.go
+++ internal/replicator/model.go
@@ -1,40 +1,29 @@
 package replicator
 
-import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-)
-
-type SourceSpec struct {
+type Source struct {
 	Name            string `json:"name"`
 	ReplicationType string `json:"replicationType"`
 }
 
-type TargetSpec struct {
+type Target struct {
 	Name            string `json:"name"`
 	ReplicationType string `json:"replicationType"`
 }
 
-type ArtifactSpec struct {
+type Artifact struct {
 	Name    string `json:"name"`
 	Version string `json:"version"`
 	Repo    string `json:"repo"`
 }
 
 type Replication struct {
-	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata,omitempty"`
-
-	Spec ReplicationSpec `json:"spec,omitempty"`
-}
-
-type ReplicationSpec struct {
-	Customer        string         `json:"customer"`
-	ReplicationType string         `json:"replicationType"`
-	VaultURI        string         `json:"vaultURI"`
-	TargetRegistry  string         `json:"targetRegistry"`
-	SourceRegistry  string         `json:"sourceResgistry"`
-	Source          SourceSpec     `json:"source"`
-	Target          TargetSpec     `json:"target"`
-	Images          []ArtifactSpec `json:"images"`
-	Charts          []ArtifactSpec `json:"charts"`
+	Customer        string     `json:"customer"`
+	ReplicationType string     `json:"replicationType"`
+	VaultURI        string     `json:"vaultURI"`
+	TargetRegistry  string     `json:"targetRegistry"`
+	SourceRegistry  string     `json:"sourceResgistry"`
+	Source          Source     `json:"source"`
+	Target          Target     `json:"target"`
+	Images          []Artifact `json:"images"`
+	Charts          []Artifact `json:"charts"`
 }
