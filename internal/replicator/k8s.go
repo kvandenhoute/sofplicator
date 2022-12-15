c75a9629fdfb451dcd6047e34e626b13b8169fc8
Karel Vanden Houte <karel.vandenhoute@outlook.com>
2022-12-15 21:15:41 +0100

    remove traces

--- internal/replicator/k8s.go
+++ internal/replicator/k8s.go
@@ -32,8 +32,6 @@ func CreateSecret(name string, uuid string, username string, password string, na
 			"AZ_ACR_PASSWORD": password,
 		},
 	}
-	log.Trace(secret)
-	log.Trace(*secret)
 	_, err := secrets.Create(context.TODO(), secret, metav1.CreateOptions{})
 	if err != nil {
 		return "", err
@@ -68,8 +66,6 @@ func CreateConfigmap(name string, uuid string, images []Artifact, charts []Artif
 			"charts.json": string(chartsJson),
 		},
 	}
-	log.Trace(configMap)
-	log.Trace(*configMap)
 	_, err = configMaps.Create(context.TODO(), configMap, metav1.CreateOptions{})
 	if err != nil {
 		return "", err
@@ -170,8 +166,6 @@ func CreateJob(name string, uuid string, image string, namespace string, imagePu
 			BackoffLimit: &backOffLimit,
 		},
 	}
-	log.Trace(job)
-	log.Trace(*job)
 	_, err := jobs.Create(context.TODO(), job, metav1.CreateOptions{})
 	if err != nil {
 		return "", nil
